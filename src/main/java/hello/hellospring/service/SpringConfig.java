73bf85f02a38dc58ea2eccce7f551c11bd558bcb
kimbongsoo <113885719+kimbongsoo@users.noreply.github.com>
2022-12-24 22:00:13 +0900

    spring_day2

---
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -0,0 +1,36 @@
+package hello.hellospring.service;
+
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        //return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcMemberRepository(dataSource);
+    }
+
+}
